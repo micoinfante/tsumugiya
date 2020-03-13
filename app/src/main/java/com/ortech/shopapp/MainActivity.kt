87a258f444f8f7b6f4c2ca47b9986b3612352035
Mico Infante <micoinfante1997@gmail.com>
2020-03-13 15:38:56 +0800

    Added notification service

--- app/src/main/java/com/ortech/shopapp/MainActivity.kt
+++ app/src/main/java/com/ortech/shopapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ortech.shopapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,12 @@ class MainActivity : AppCompatActivity() {
 
 //        val intent = Intent(this, BottomNavigationActivity::class.java)
 //        startActivity(intent)
+        var builder = NotificationCompat.Builder(this, 999.toString())
+            .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+            .setContentTitle("This is a test title")
+            .setContentText("This is a test content")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
         val intent = Intent(this, HomeScreen::class.java)
         startActivity(intent)
     }
