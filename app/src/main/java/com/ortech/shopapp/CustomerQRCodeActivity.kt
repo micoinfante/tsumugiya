ab7351ef9b9cee0dddb30e2a0f17c3d0829d6e3e
mik0153 <khiel1315@gmail.com>
2020-04-06 23:04:42 +0800

    Added QRCode tab for transferring and redeeming coupons. Added Tab Section for store list.

---
+++ app/src/main/java/com/ortech/shopapp/CustomerQRCodeActivity.kt
@@ -0,0 +1,12 @@
+package com.ortech.shopapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerQRCodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_qr_code)
+    }
+}
