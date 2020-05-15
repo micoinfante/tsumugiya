4f007c579307a0b7df08e952fc88cfe6259b573c
mik0153 <khiel1315@gmail.com>
2020-05-15 20:20:28 +0800

    * Added connection to branch coupon list to coupon details

--- app/src/main/java/com/ortech/shopapp/Adapters/AllCouponListAdapter.kt
+++ app/src/main/java/com/ortech/shopapp/Adapters/AllCouponListAdapter.kt
@@ -9,9 +9,12 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.common.io.Resources.getResource
+import com.ortech.shopapp.BranchCouponList
+import com.ortech.shopapp.CouponDetails
 import com.ortech.shopapp.Models.Coupon
 import com.ortech.shopapp.R
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_branch_coupon_item.view.*
 import kotlinx.android.synthetic.main.fragment_home_fifth_section.view.*
 import kotlinx.android.synthetic.main.fragment_home_fourth_section.view.*
 import kotlinx.android.synthetic.main.fragment_home_screen_item.view.*
@@ -33,11 +36,26 @@ class AllCouponListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
   override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
     if (holder is CouponViewHolder) {
-
+      holder.bind()
     }
   }
 
   inner class CouponViewHolder constructor(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val couponName = itemView.textViewCouponItemName
+    val timestamp = itemView.textViewCouponItemTimestamp
+    val requiredPoints = itemView.textViewCouponItemPoints
+
+    fun bind() {
+      itemView.setOnClickListener {
+        val activity = itemView.context as AppCompatActivity
+        val fragment = CouponDetails.newInstance("CouponName1", "12345")
+        val transaction =  activity.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+      }
+
+    }
 
   }
 
--- app/src/main/java/com/ortech/shopapp/CouponDetails.kt
+++ app/src/main/java/com/ortech/shopapp/CouponDetails.kt
@@ -1,15 +1,20 @@
 package com.ortech.shopapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_branch_coupon_item.*
+import kotlinx.android.synthetic.main.fragment_coupon_details.*
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_COUPON_NAME = "couponName"
+private const val ARG_COUPON_TIMESTAMP = "couponTimestamp"
+private const val ARG_COUPON_POINTS = "couponPoints"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,14 +23,21 @@ private const val ARG_PARAM2 = "param2"
  */
 class CouponDetails : Fragment() {
   // TODO: Rename and change types of parameters
-  private var param1: String? = null
-  private var param2: String? = null
+  private var couponName: String? = null
+  private var couponTimestamp: String? = null
+  private var couponPoint: Int? = null
+
+//  private var textViewCouponName = textViewCouponDetailsName
+//  private var textViewTimestamp = textViewCouponDetailsTimestamp
+//  private var textViewPoints = textViewCouponDetailsPoints
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     arguments?.let {
-      param1 = it.getString(ARG_PARAM1)
-      param2 = it.getString(ARG_PARAM2)
+      couponName = it.getString(ARG_COUPON_NAME)
+      couponTimestamp = it.getString(ARG_COUPON_TIMESTAMP)
+      couponPoint = it.getInt(ARG_COUPON_POINTS)
+
     }
   }
 
@@ -35,6 +47,15 @@ class CouponDetails : Fragment() {
   ): View? {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_coupon_details, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    textViewCouponDetailsName.text = couponName
+    textViewCouponDetailsTimestamp.text = couponTimestamp
+    textViewCouponDetailsPoints.text = couponPoint.toString()
+
   }
 
   companion object {
@@ -51,8 +72,9 @@ class CouponDetails : Fragment() {
     fun newInstance(param1: String, param2: String) =
       CouponDetails().apply {
         arguments = Bundle().apply {
-          putString(ARG_PARAM1, param1)
-          putString(ARG_PARAM2, param2)
+          putString(ARG_COUPON_NAME, couponName)
+          putString(ARG_COUPON_TIMESTAMP, couponTimestamp)
+          couponPoint?.let { putInt(ARG_COUPON_POINTS, it) }
         }
       }
   }
